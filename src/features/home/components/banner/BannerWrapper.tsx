322b44c61b5f83fe8ac69887b1cf1cd7e6dae24c
Adil-Akothiat <adilrar156@gmail.com>
2025-12-18 13:09:33 +0100

    Next.js does NOT resolve '.' reliably in app router issue solved

--- src/features/home/components/banner/BannerWrapper.tsx
+++ src/features/home/components/banner/BannerWrapper.tsx
@@ -1,6 +1,6 @@
 import dynamic from "next/dynamic";
 
-const BannerComponent = dynamic(() => import("."), {
+const BannerComponent = dynamic(() => import("./index"), {
   ssr: false,
   loading: () => <div className="w-full h-96 bg-gray-200 animate-pulse" />,
 });
